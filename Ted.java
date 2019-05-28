4000bcff4502c522c9ae5f34c1842fe21717e44e
Ksyusha <43499411+Ksyusha7650@users.noreply.github.com>
2019-05-29 00:17:30 +0300

    Update Ted.java

--- Ted.java
+++ Ted.java
@@ -6,24 +6,26 @@ class Ted extends Elements {
     public Ted(Context context) {
         bitmapId = R.raw.ted; // определяем начальные параметры
         size = 5;
+        height =4;
+        width = 3;
         x=7;
-        y=GameView.maxY - size - 1;
+        y=12;
         speed = (float) 0.2;
 
         init(context);
     }
     @Override
-    public void update() { // перемещаем мишку в зависимости от нажатой кнопки
+    public void update() {// перемещаем мишку в зависимости от нажатой кнопки
         if(MainActivity.isLeftPressed && x >= 0){
             x -= speed;
         }
-        if(MainActivity.isRightPressed && x <= GameView.maxX - 5){
+        if(MainActivity.isRightPressed && x <= GameView.maxX){
             x += speed;
         }
         if(MainActivity.isUpPressed && y >= 0){
-            y -= speed;
+            y -=speed;
         }
-        if(MainActivity.isDownPressed && y <= GameView.maxY - 5){
+        if(MainActivity.isDownPressed && y <= GameView.maxY){
             y += speed;
         }
 
