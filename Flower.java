49eb41f526ad96ac3c5ffc5e8b28a43f794aec46
Ksyusha <43499411+Ksyusha7650@users.noreply.github.com>
2019-05-29 00:19:15 +0300

    Update Flower.java

--- Flower.java
+++ Flower.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 class Flower extends Elements {
     private int radius = 2; // радиус
+    private float minSpeed = (float) 0.1; // минимальная скорость
+    private float maxSpeed = (float) 0.5;
 
 
     public Flower(Context context) {
@@ -14,9 +16,10 @@ class Flower extends Elements {
         bitmapId = R.raw.flower;
         y=0;
         x = random.nextInt(GameView.maxX) - radius;
-        //size = radius*2;
-//        speed = minSpeed + (maxSpeed - minSpeed) * random.nextFloat();
+
          speed = 1;
+         height = 1;
+         width = 1;
          size = 2;
         init(context);
     }
@@ -25,7 +28,10 @@ class Flower extends Elements {
     public void update() {
         y += speed;
     }
-    public boolean isCollision(float tedX, float tedY, float tedSize) {
-        return !(((x+size) < tedX)||(x > (tedX+tedSize))||((y+size) < tedY)||(y > (tedY+tedSize)));
+    public boolean isCollision(float tedX, float tedY, float tedHeight, float tedWidth) {
+        if (((x+size) < tedX)||(x > (tedX+tedWidth))||((y+size) < tedY)||(y > (tedY+tedHeight))){
+            return true;
+        }
+        else return false;
     }
 }
