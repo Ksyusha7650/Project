f845b88b505905991fe9f78889bc1b1a5e861c7e
Ksyusha <43499411+Ksyusha7650@users.noreply.github.com>
2019-05-29 00:18:15 +0300

    Update Hill.java

--- Hill.java
+++ Hill.java
@@ -3,15 +3,18 @@ package com.example.game;
 import android.content.Context;
 
 public class Hill extends Elements  {
-
+public int floorY = 3;
 
 
     public Hill(Context context) {
-        bitmapId = R.raw.hill;
-        y =GameView.maxY - 40;
-        size = 3;
+        bitmapId = R.raw.hill1;
+        size = 5;
+        y=15;
+       // height = (int) ((Math.random()*GameView.maxY)/10 + (0.3)*GameView.maxY);
+        height = 3;
+        width = 3;
         x=0;
-        speed = 1;
+        speed = 0.5f;
         init(context);
 
     }
@@ -20,17 +23,9 @@ public class Hill extends Elements  {
         x += speed;
     }
     public boolean isCollision(float tedX, float tedY, float tedSize) {
-        return !(((x+size) < tedX)||(x > (tedX+tedSize))||((y+size) < tedY)||(y > (tedY+tedSize)));
+        return !(((x+width) < tedX)||(x > (tedX+tedSize))||((y+size) < tedY)||(y > (tedY+tedSize)));
     }
-    public Hill(int x, int y, int height, int width){
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.width = width;
-    }
-    public String toString() {
-        return getClass().getName() + "[x=" + x + ",y=" + y + ",width=" + width + ",height=" + height + "]";
-    }
+
 }
 
 
